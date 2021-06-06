1cc0e6fffa971ea9c36298368843f1a3ab970376
Sofia Tejerina <sofiatejerina23@gmail.com>
2021-06-05 23:33:02 -0300

    Revert "Feature/t2 u1 (#4)" (#11)
    This reverts commit 41ce22c4311014c66fc3d9dd02488d7bf1967a3d.

--- app/src/main/java/com/example/biladoniga_toscanini_tejerina/commons/TeamStatus.kt
+++
@@ -1,5 +0,0 @@
-package com.example.biladoniga_toscanini_tejerina.commons
-
-enum class TeamStatus {
-    READY, ALMOST_READY, NO_READY
-}
--- data/src/main/java/com/data/repositories/TeamRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.data.repositories
-
-import com.data.Response
-import com.data.models.Team
-
-interface TeamRepository {
-    fun getTeam(teamId: String, cBack: (Response<Team>) -> Unit)
-}
